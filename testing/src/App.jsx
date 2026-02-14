225e972eff23624da1678204b50b274a0e51702d
Vaishvik-Jaiswal <vaishvikjaiswal05726@gmail.com>
2026-02-14 13:38:46 +0530

    tp

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <main className="app">
       <h1>Testing</h1>
-      <p className="subtitle">Simple React frontend</p>
+      <p className="subtitle">Simple React frontend Vaishvik</p>
       <div className="card">
         <button
           type="button"
